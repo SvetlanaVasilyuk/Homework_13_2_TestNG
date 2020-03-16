eed054bd26438ac1eff474aef4b95e3095e98c49
Svetlana Vasilyuk <lanarusufa@gmail.com>
2020-03-16 15:57:04 +0300

    Init commit

---
+++ src/main/java/com/github/Svetlana/Vasilyuk/App.java
@@ -0,0 +1,13 @@
+package com.github.Svetlana.Vasilyuk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
